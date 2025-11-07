9068773310b94266f028b84660f8e185f22456ac
Matus Gavornik <mgavorni@c2r9s3.42prague.com>
2025-11-07 18:28:58 +0100

    jebem ti boha

--- cpp/ex01/contact.cpp
+++ cpp/ex01/contact.cpp
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgavornik <mgavornik@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mgavorni <mgavorni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 17:34:04 by mgavornik         #+#    #+#             */
-/*   Updated: 2025/11/07 17:34:44 by mgavornik        ###   ########.fr       */
+/*   Updated: 2025/11/07 18:01:26 by mgavorni         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "contact.hpp"
 
@@ -21,3 +21,16 @@ Contact::~Contact()
 {
     std::cout << "Contact Destructor" << std::endl;
 }
+
+void Contact::setContact(st_str first_name,
+                st_str last_name, 
+                st_str nickname, 
+                st_str phone_number, 
+                st_str darkest_secret)
+                {
+                    _first_name = first_name;
+                    _last_name = last_name;
+                    _nickname = nickname;
+                    _phone_number = phone_number;
+                    _darkest_secret = darkest_secret;
+                }
--- cpp/ex01/phonebook.cpp
+++ cpp/ex01/phonebook.cpp
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgavornik <mgavornik@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mgavorni <mgavorni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 14:34:40 by mgavorni          #+#    #+#             */
-/*   Updated: 2025/11/07 17:39:00 by mgavornik        ###   ########.fr       */
+/*   Updated: 2025/11/07 18:28:35 by mgavorni         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 
 #include "phonebook.hpp"
@@ -27,11 +27,50 @@ void PhoneBook::errorHandler(void)
 {
     std::cout << "Unknown command" << std::endl;
 }
+void PhoneBook::addContact(void) 
+{
+    Fields fields;
+    int i;
+    
+    std::cout << "First name: ";
+    std::string first_name;
+    fields = FIRST_NAME;
+    std::getline(std::cin, first_name);
+    i = FIRST_NAME;
+
+    std::cout << "Last name: ";
+    std::string last_name;
+    fields = LAST_NAME;
+    std::getline(std::cin, last_name);
+    i = LAST_NAME;
+    
+    std::cout << "Nickname: ";
+    std::string nickname;
+    fields = NICKNAME;
+    std::getline(std::cin, nickname);
+    i = NICKNAME;
+    
+    std::cout << "Phone number: ";
+    std::string phone_number;
+    fields = PHONE_NUMBER;
+    std::getline(std::cin, phone_number);
+    i = PHONE_NUMBER;
+    
+    std::cout << "Darkest secret: ";
+    std::string darkest_secret;
+    fields = DARKEST_SECRET;
+    std::getline(std::cin, darkest_secret);
+    i = DARKEST_SECRET;
+    if(i ==  )
+        processCmdEXIT(EXIT);
+    
+}
 
 void PhoneBook::processCmdADD(Commands Cmd)
 {
     std::cout << Cmd << std::endl;
-    std::cout << "ADD";
+
+    addContact();
 }
 void PhoneBook::processCmdSEARCH(Commands Cmd)
 {
