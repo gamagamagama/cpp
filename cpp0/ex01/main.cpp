e2e5d20a4d6d67841a60f9131ba4e9d5674dc1e2
Matus Gavornik <mgavorni@c3r5s3.42prague.com>
2025-11-12 14:18:27 +0100

    added exercises

---
+++ cpp0/ex01/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgavorni <mgavorni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 15:34:56 by mgavorni          #+#    #+#             */
+/*   Updated: 2025/11/11 19:08:24 by mgavorni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+
+int main(void)
+{
+    PhoneBook PB;
+    std::string input;
+    while(true){
+        std::cout << "Enter command: ";
+        std::getline(std::cin, input);
+        if(input.compare("EXIT") == 0)
+            break;
+        PB.processCmd(input);
+    }
+    return(0);
+}
