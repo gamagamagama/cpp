eb2a9c3e041b4e85b0ff0c564e906eb701b3da9b
Matus Gavornik <mgavorni@c3r5s3.42prague.com>
2025-11-12 17:22:03 +0100

    Horde create

---
+++ cpp1/ex01/zombieHorde.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgavorni <mgavorni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 15:38:59 by mgavorni          #+#    #+#             */
+/*   Updated: 2025/11/12 16:44:23 by mgavorni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie* zombieHorde(int N, std::string name)
+{
+    if (N <= 0) return NULL;
+    Zombie* horde = new Zombie[N];          
+    for (int i = 0; i < N; ++i)
+        horde[i].addName(name);
+    return horde;  
+}
